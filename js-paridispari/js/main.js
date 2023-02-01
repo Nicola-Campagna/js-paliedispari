add9d218efc694fdcc887908682a3d05247e3db6
Nicola-Campagna <campagnanicola379@gmail.com>
2023-02-01 14:10:02 +0100

    refactoring js-pari-dispari

--- js-paridispari/js/main.js
+++ js-paridispari/js/main.js
@@ -4,38 +4,57 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+const minValue = 1
+const maxValue = 5;
 
+let userChoice = prompt('Scegli "pari" o "dispari"');
+if (
+    userChoice != "pari" &&
+    userChoice != "dispari") {
 
-// L'utente sceglie pari o dispari
-const userPari = document.getElementById("button-pari");
-const userDispari = document.getElementById("button-dispari");
-// numero minimo e numero massimo
-const min = 1;
-const max = 5;
+    userChoice = "pari";
+    alert("Ho scelto pari per te");
+    console.log("all'utente è stato assegnato: " + userChoice);
 
-// utente inserisce un numero da 1 a 5.
-let userNumber = parseInt(Math.floor((Math.random() * max) + min));
-console.log("numero utente: " + userNumber);
-
-// funzione per richiamare un numero random da 1 a 5 assegnato al pc
-
-randomPcNumber();
-const valore1 = userNumber;
-// const valore2 = pcNumber;
-// funzione per richiamare la somma dei numeri assegnati ai due giocatori
-somma(valore1, valore2);
-
-function randomPcNumber(valore2) {
-    let pcNumber = parseInt(Math.floor((Math.random() * max) + min));
-    valore2 = parseInt(valore2);
-    console.log("numero pc: " + pcNumber);
-    return;
+} else {
+    console.log("l'utente ha scelto: " + userChoice);
 }
 
-function somma(num1, num2) {
-    num1 = userNumber(num1);
-    num2 = pcNumber(num2);
-    const risultato = num1 + num2;
-    // console.log("somma dei due numeri: " + risultato);
-    return risultato;
+let userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+if (userNumber > maxValue || userNumber < minValue || userChoice == !isNaN) {
+    alert("numero inserito non valido");
+    randomUserNumber = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+    userNumber = randomUserNumber;
+    alert("ho scelto io per te: " + randomUserNumber)
+    console.log("numero random assegnato all'utente è " + userNumber);
+}
+else {
+    console.log("numero assegnato all'utente: " + userNumber);
+}
+// numero random asseganto al pc
+console.log("numero assegnato al pc: " + (generatePcNumber(1, 5)));
+
+// SOMMA DEI DUE NUMERI
+const gameTotal = userNumber + (generatePcNumber(1, 5));
+console.log("somma dei due numeri assegati = " + gameTotal);
+
+// decretare il vincitore:
+if ((isEven(gameTotal)) && userChoice == "pari" || (!isEven(gameTotal)) && userChoice == "dispari") {
+    console.log("ha vinto utente")
+}
+else {
+    console.log("havinto pc");
+}
+
+
+// richiamola funzione per vedere che numero è stato generato e assegnato al pc
+
+// funzione che genera un numero compreso da 1 a 5 e lo assegna al pc
+function generatePcNumber(min, max) {
+    const randomPcNumber = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+    return randomPcNumber;
+}
+// funzione che verifica se la somma dei due numeri asseganti sia pari
+function isEven(num) {
+    return num % 2 == 0;
 }
